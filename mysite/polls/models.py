8f11a95c7654521dbd760633d1d291cde672bfec
Joe Johnson <joe.wendell.johnson@gmail.com>
2017-03-25 12:10:02 -0500

    Finished beginner tutorial

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 # Create your models here.
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +10,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
